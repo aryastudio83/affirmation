c9a3d52a6799c300c5768b1fb2461b4173207892
aryastudio <aryastudio83@gmail.com>
2022-05-22 22:56:45 +0700

    add images

--- app/src/main/java/com/aryastudio83/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/aryastudio83/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.aryastudio83.affirmations.R
@@ -13,6 +14,7 @@ import com.aryastudio83.affirmations.model.Affirmation
 class ItemAdapter(private val context: Context, private val dataSet: List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -22,6 +24,7 @@ class ItemAdapter(private val context: Context, private val dataSet: List<Affirm
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataSet[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int {
